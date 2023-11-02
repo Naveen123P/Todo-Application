e11067cff5adc67bd2c6115491445a1d3f5fe099
Naveen123P <125892820+Naveen123P@users.noreply.github.com>
2023-11-02 15:25:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+module.exports = app;
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
@@ -23,10 +24,55 @@ const initilizeDBAndServer = async () => {
 };
 initilizeDBAndServer();
 
-// API
-app.post("/", async (request, response) => {
+// API 1
+app.("/", async (request, response) => {
+
   const createTodoTableQuary = `
         
     `;
   response.send("");
 });
+
+// API 2
+
+app.get("/todos/:todoId/", async (request, response)=>{
+    const {todoId} = request.params
+    const getTodosQuary = `
+    SELECT * FROM todo WHERE id = ${todoId};
+    `
+    const getTodos = await db.all(getTodosQuary)
+    response.send(getTodos)
+})
+
+// API 3
+
+app.post("/todos/", await(request, response)=>{
+    const {todo, priority, status} = request.body
+    const postTodoQuary =  `
+    INSERT INTO 
+        todo(todo, priority, status)
+    VALUES
+        ('${todo}','${priority}', '${status}');
+    `
+    const updatedTodoTable = await db.run(postTodoQuary);
+    response.send("Todo Successfully Added")
+})
+
+// API 4 
+
+
+
+
+// API 5 
+
+app.delete("/todos/:todoId/", async (request, response)=>{
+    const {todoId} = request.params
+    const deleteTodoQuary = `
+    DELETE FROM 
+        todo 
+    WHERE 
+        id = ${todoId};
+    `
+    const deletedTodoTable = await db.run(deleteTodoQuary)
+    response.send("Todo Deleted")
+})
